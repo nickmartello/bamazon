d209caff630761ce4647daa8a6ba67a18758f30d
Nick Martello <thenickmartello@gmail.com>
2019-12-11 15:34:49 -0500

    added some sugar

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -3,19 +3,19 @@ let mysql = require('mysql');
 
 
 let connection = mysql.createConnection({
-	host: 'localhost',
-	port: 3306,
-	user: 'root',
-	password: 'Hammerdick1',
-	database: 'bamazonDB'
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'Hammerdick1',
+    database: 'bamazonDB'
 });
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw err;
     console.log("sweet");
     // run the start function after the connection is made to prompt the user
     start();
-  });
-  function start() {
+});
+function start() {
     connection.query(`SELECT * FROM bamazonDB.products`, function (err, res) {
         if (err) throw err;
         for (i = 0; i < res.length; i++) {
@@ -29,49 +29,54 @@ connection.connect(function(err) {
             display('Quantity: ', res[i].stock_quantity);
 
             display('\n', '');
-        };  
-        function determineS(value) {
-            if(value > 1) {
-                return 's';
-            } else {
-                return '';
-            };
+        };
+
+
+        //function determineS(value)
+
+            // fat arrrow function
+
+        const determineS = value => {
+
+            // ternary function, the question mark is the if and the colon is the else
+             return value >= 1 ? 's' : '';
         };
         inquirer
-        .prompt([
-            {
-                type: `input`,
-                message: `What product would you like to purchase? (enter id #)`,
-                name: `product`
-            },
-            {
-                type: `input`,
-                message: `How many would you like to purchase?`,
-                name: `quantity`
-            }
-        ])
-        .then(function (reply) {
-            connection.query(`SELECT * FROM bamazonDB.products WHERE ?`, { item_id: reply.product }, function (err, res) {
-                if (err) throw err;
-                if (parseInt(reply.quantity) <= parseInt(res[0].stock_quantity)) {
-                    let newQuan = parseInt(res[0].stock_quantity) - parseInt(reply.quantity);
-                    connection.query(`UPDATE bamazonDB.products SET ? WHERE ?`, [{ stock_quantity: newQuan }, { item_id: reply.product }], function (err, res) {
-                        if (err) throw err;
-                    });
-                    connection.query(`SELECT * FROM bamazonDB.products WHERE ?`, { item_id: reply.product }, function (err, res) {
-                        if (err) throw err;
-                        console.log(`\n${res[0].stock_quantity} ${res[0].product_name}${determineS(res[0].stock_quantity)} left in stock...\n`)
-                        console.log(`You paid $${(parseInt(reply.quantity) * parseFloat(res[0].price)).toFixed(2)} for ${parseInt(reply.quantity)} ${res[0].product_name}${determineS(parseInt(reply.quantity))}\n`);
+            .prompt([
+                {
+                    type: `input`,
+                    message: `What product would you like to purchase? (enter id #)`,
+                    name: `product`
+                },
+                {
+                    type: `input`,
+                    message: `How many would you like to purchase?`,
+                    name: `quantity`
+                }
+            ])
+            .then(function (reply) {
+                connection.query(`SELECT * FROM bamazonDB.products WHERE ?`, { item_id: reply.product }, function (err, res) {
+                    if (err) throw err;
+                    if (parseInt(reply.quantity) <= parseInt(res[0].stock_quantity)) {
+                        let newQuantity = parseInt(res[0].stock_quantity) - parseInt(reply.quantity);
+                        connection.query(`UPDATE bamazonDB.products SET ? WHERE ?`, [{ stock_quantity: newQuantity }, { item_id: reply.product }], function (err, res) {
+                            if (err) throw err;
+                        });
+                        connection.query(`SELECT * FROM bamazonDB.products WHERE ?`, { item_id: reply.product }, function (err, res) {
+                            if (err) throw err;
+                            console.log(`\n${res[0].stock_quantity} ${res[0].product_name}${determineS(res[0].stock_quantity)} left in stock...\n`)
+                            
+                            console.log(`You paid $${(parseInt(reply.quantity) * parseFloat(res[0].price)).toFixed(2)} for ${parseInt(reply.quantity)} ${res[0].product_name}${determineS(parseInt(reply.quantity))}\n`);
+                            connection.end();
+                        })
+                        return;
+                    } else {
+                        console.log(`Insufficent quantity!`);
                         connection.end();
-                    })
-                    return;
-                } else {
-                    console.log(`Insufficent quantity!`);
-                    connection.end();
-                };
+                    };
+                });
             });
-        });
-});
+    });
 
 };
-     
+
