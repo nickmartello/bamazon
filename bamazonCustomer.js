9d2f8e4c18c67b829671a8e5925335c90d381901
Nick Martello <thenickmartello@gmail.com>
2019-12-11 18:35:59 -0500

    too much syntactical sugar

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -11,35 +11,35 @@ let connection = mysql.createConnection({
 });
 connection.connect(function (err) {
     if (err) throw err;
-    console.log("sweet");
+    console.log("Welcome to my Bamazon store");
     // run the start function after the connection is made to prompt the user
     start();
 });
 function start() {
     connection.query(`SELECT * FROM bamazonDB.products`, function (err, res) {
         if (err) throw err;
-        for (i = 0; i < res.length; i++) {
+        console.table(res)
+        for (let i = 0; i < res.length; i++) {
             function display(string, value) {
-                console.log(string + value);
+                
             };
             display('ID: ', res[i].item_id);
             display('Product: ', res[i].product_name);
             display('Department: ', res[i].department_name);
             display('Price: $', (res[i].price).toFixed(2));
             display('Quantity: ', res[i].stock_quantity);
-
             display('\n', '');
         };
 
 
-        //function determineS(value)
+        //function addS(value)
 
             // fat arrrow function
 
-        const determineS = value => {
+        const addS = value => {
 
             // ternary function, the question mark is the if and the colon is the else
-             return value >= 1 ? 's' : '';
+             return value > 1 ? 's' : '';
         };
         inquirer
             .prompt([
@@ -64,9 +64,9 @@ function start() {
                         });
                         connection.query(`SELECT * FROM bamazonDB.products WHERE ?`, { item_id: reply.product }, function (err, res) {
                             if (err) throw err;
-                            console.log(`\n${res[0].stock_quantity} ${res[0].product_name}${determineS(res[0].stock_quantity)} left in stock...\n`)
-                            
-                            console.log(`You paid $${(parseInt(reply.quantity) * parseFloat(res[0].price)).toFixed(2)} for ${parseInt(reply.quantity)} ${res[0].product_name}${determineS(parseInt(reply.quantity))}\n`);
+                            console.log(`\n${res[0].stock_quantity} ${res[0].product_name}${addS(res[0].stock_quantity)} left in stock...\n`)
+
+                            console.log(`You paid $${(parseInt(reply.quantity) * parseFloat(res[0].price)).toFixed(2)} for ${parseInt(reply.quantity)} ${res[0].product_name}${addS(parseInt(reply.quantity))}\n`);
                             connection.end();
                         })
                         return;
